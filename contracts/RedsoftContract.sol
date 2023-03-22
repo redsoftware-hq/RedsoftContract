0e16b130d2bc4e96fc0edfd2b39cd71543e9ad41
Vibek Dutta <vikiisbck@gmail.com>
2023-03-23 00:33:44 +0530

    Add : Fetch all minted nfts and user minted nfts functions added

--- contracts/RedsoftContract.sol
+++ contracts/RedsoftContract.sol
@@ -20,16 +20,29 @@ contract RedsoftContract is ERC721URIStorage {
         uint256 tokenId;
         address payable sellerOfNft;
         address payable ownerOfNft;
+        bool isListed;
         uint256 price;
         bool sold;
     }
 
-    event NftCreatedEvent(
+    event NftMintedEvent(
         uint256 indexed tokenId,
-        address sellerOfNft,
-        address ownerOfNft,
+        address indexed sellerOfNft,
+        address indexed ownerOfNft,
+        bool isListed,
         uint256 price,
         bool sold
+    );
+    event NFTListedEvent(
+        uint256 price,
+        uint256 indexed tokenId,
+        address indexed oldOwnerOfnft
+    );
+    event NFTSoldEvent(
+        uint256 price,
+        uint256 indexed tokenId,
+        address indexed oldOwnerOfnft,
+        address indexed newOwnerOfNft
     );
 
     constructor() ERC721("Redsoft Tokens", "RST") {
@@ -50,31 +63,32 @@ contract RedsoftContract is ERC721URIStorage {
         return listingPrice;
     }
 
-    // Mints a token and lists it in the marketplace
-    function createToken(
-        string memory tokenURI
-    ) public payable returns (uint256) {
+    // Mints a token
+    function mintToken(string memory tokenURI) public returns (uint256) {
         _tokenIds.increment();
         uint256 newTokenId = _tokenIds.current();
 
         _mint(msg.sender, newTokenId);
         _setTokenURI(newTokenId, tokenURI);
 
-        require(
-            msg.value == listingPrice,
-            "You have to pay the listing price to mint"
-        );
-
         tokenIdToNftObject[newTokenId] = Nft(
             newTokenId,
             payable(msg.sender),
-            payable(address(this)),
+            payable(address(0)),
+            false,
             0,
             false
         );
 
-        _transfer(msg.sender, address(this), newTokenId);
-        emit NftCreatedEvent(newTokenId, msg.sender, address(this), 0, false);
+        emit NftMintedEvent(
+            newTokenId,
+            msg.sender,
+            address(0),
+            false,
+            0,
+            false
+        );
+
         return newTokenId;
     }
 
@@ -90,45 +104,56 @@ contract RedsoftContract is ERC721URIStorage {
         );
         tokenIdToNftObject[tokenId].sold = false;
         tokenIdToNftObject[tokenId].price = price;
+        tokenIdToNftObject[tokenId].isListed = false;
         tokenIdToNftObject[tokenId].sellerOfNft = payable(msg.sender);
         tokenIdToNftObject[tokenId].ownerOfNft = payable(address(this));
+
         _itemsSold.decrement();
 
         _transfer(msg.sender, address(this), tokenId);
     }
 
-    // Transfers ownership of the item, as well as funds between parties
+    // Lists a token
     function listNft(uint256 tokenId, uint256 price) public payable {
         require(_exists(tokenId), "Token does not exist");
         require(
             msg.sender == tokenIdToNftObject[tokenId].sellerOfNft,
             "Only token minter can list the token"
         );
+        require(
+            msg.value == listingPrice,
+            "You have to pay the listing price to mint"
+        );
         require(price > 0, "Price must be greater than 0");
 
-        address seller = tokenIdToNftObject[tokenId].sellerOfNft;
-
-        tokenIdToNftObject[tokenId].ownerOfNft = payable(msg.sender);
         tokenIdToNftObject[tokenId].sold = false;
-        tokenIdToNftObject[tokenId].sellerOfNft = payable(address(0));
         tokenIdToNftObject[tokenId].price = price;
+        tokenIdToNftObject[tokenId].isListed = true;
+        tokenIdToNftObject[tokenId].ownerOfNft = payable(msg.sender);
+        tokenIdToNftObject[tokenId].sellerOfNft = payable(msg.sender);
+
         _itemsSold.increment();
-        _transfer(address(this), msg.sender, tokenId);
-        payable(ownerOfContract).transfer(listingPrice);
-        payable(seller).transfer(msg.value);
+
+        emit NFTListedEvent(0, tokenId, msg.sender);
+
+        payable(ownerOfContract).transfer(msg.value);
     }
 
     // Returns all minted market items of user
-    function fetchMintedItemsOfUser(
-        address owner
+    function fetchNFTsMintedUser(
+        address user
     ) public view returns (Nft[] memory) {
-        uint256 tokenCount = balanceOf(owner);
+        uint256 tokenCount = balanceOf(user);
 
         Nft[] memory nfts = new Nft[](tokenCount);
         uint256 j = 0;
 
         for (uint256 i = 1; i <= _tokenIds.current(); i++) {
-            if (_exists(i) && ownerOf(i) == owner) {
+            if (
+                _exists(i) &&
+                ownerOf(i) == user &&
+                tokenIdToNftObject[i].sold == false
+            ) {
                 Nft memory nft = tokenIdToNftObject[i];
                 nfts[j] = nft;
                 j++;
@@ -139,7 +164,7 @@ contract RedsoftContract is ERC721URIStorage {
     }
 
     // Returns all unsold market items
-    function fetchMarketItems() public view returns (Nft[] memory) {
+    function fetchAllUnsoldNFTs() public view returns (Nft[] memory) {
         uint256 itemCount = _tokenIds.current();
         uint256 unsoldItemCount = _tokenIds.current() - _itemsSold.current();
         uint256 currentIndex = 0;
@@ -157,7 +182,7 @@ contract RedsoftContract is ERC721URIStorage {
     }
 
     // Returns only items that a user has purchased
-    function fetchMyNFTs() public view returns (Nft[] memory) {
+    function fetchNFTsUser() public view returns (Nft[] memory) {
         uint256 totalItemCount = _tokenIds.current();
         uint256 itemCount = 0;
         uint256 currentIndex = 0;
@@ -180,8 +205,10 @@ contract RedsoftContract is ERC721URIStorage {
         return items;
     }
 
-    // Returns only items a user has listed
-    function getItemsListed(address user) public view returns (Nft[] memory) {
+    // Returns only items that a user has listed
+    function fetchNFTsListedUser(
+        address user
+    ) public view returns (Nft[] memory) {
         uint256 totalItems = _tokenIds.current();
         uint256 itemCount = 0;
         Nft[] memory listedItems = new Nft[](totalItems);
@@ -189,7 +216,8 @@ contract RedsoftContract is ERC721URIStorage {
         for (uint256 i = 1; i <= totalItems; i++) {
             if (
                 tokenIdToNftObject[i].sellerOfNft == user &&
-                tokenIdToNftObject[i].sold == false
+                tokenIdToNftObject[i].sold == false &&
+                tokenIdToNftObject[i].isListed == true
             ) {
                 listedItems[itemCount] = tokenIdToNftObject[i];
                 itemCount++;
@@ -203,4 +231,39 @@ contract RedsoftContract is ERC721URIStorage {
 
         return userItems;
     }
+
+    // Transfers ownership of the item as well as funds between parties
+    function buyNFT(uint256 tokenId) public payable {
+        require(
+            tokenIdToNftObject[tokenId].sellerOfNft != address(0),
+            "NFT not listed for sale"
+        );
+        require(
+            msg.value == tokenIdToNftObject[tokenId].price,
+            "Incorrect amount sent"
+        );
+        address seller = tokenIdToNftObject[tokenId].sellerOfNft;
+        tokenIdToNftObject[tokenId].sellerOfNft = payable(address(0));
+        tokenIdToNftObject[tokenId].ownerOfNft = payable(msg.sender);
+        payable(seller).transfer(msg.value);
+        safeTransferFrom(ownerOf(tokenId), msg.sender, tokenId);
+    }
+
+    // Gets an NFT token details
+    // function getNFT(uint256 tokenId)
+    //     public
+    //     view
+    //     returns (
+    //         address,
+    //         address,
+    //         uint256
+    //     )
+    // {
+    //     return (
+    //         tokenIdToNftObject[tokenId].sellerOfNft,
+    //         tokenIdToNftObject[tokenId].ownerOfNft,
+    //         tokenIdToNftObject[tokenId].price,
+
+    //     );
+    // }
 }
